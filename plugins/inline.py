d92d16d1148dbccc336ec13044cb967a4951f0c6
stuartrobert474-wq <stuartrobert474@gmail.com>
2026-08-24 18:26:12 +0600

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -1,113 +1,195 @@
 import logging
-from pyrogram import Client, emoji, filters
+
+from pyrogram import Client, emoji
 from pyrogram.errors.exceptions.bad_request_400 import QueryIdInvalid
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultCachedDocument, InlineQuery
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InlineQueryResultCachedDocument,
+    InlineQuery,
+)
+
 from database.ia_filterdb import get_search_results
 from utils import is_subscribed, get_size, temp
-from info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
+from info import (
+    CACHE_TIME,
+    AUTH_USERS,
+    AUTH_CHANNEL,
+    CUSTOM_FILE_CAPTION,
+)
 
 logger = logging.getLogger(__name__)
+
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
+
 
 async def inline_users(query: InlineQuery):
     if AUTH_USERS:
         if query.from_user and query.from_user.id in AUTH_USERS:
             return True
-        else:
-            return False
+        return False
+
     if query.from_user and query.from_user.id not in temp.BANNED_USERS:
         return True
+
     return False
 
+
 @Client.on_inline_query()
-async def answer(bot, query):
-    """Show search results for given inline query"""
-    
+async def answer(bot, query: InlineQuery):
+    """Show search results for given inline query."""
+
+    # Check user permission
     if not await inline_users(query):
-        await query.answer(results=[],
-                           cache_time=0,
-                           switch_pm_text='okDa',
-                           switch_pm_parameter="hehe")
+        await query.answer(
+            results=[],
+            cache_time=0,
+            switch_pm_text="okDa",
+            switch_pm_parameter="hehe",
+        )
         return
 
+    # Check channel subscription
     if AUTH_CHANNEL and not await is_subscribed(bot, query):
-        await query.answer(results=[],
-                           cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
-                           switch_pm_parameter="subscribe")
+        await query.answer(
+            results=[],
+            cache_time=0,
+            switch_pm_text="You have to subscribe my channel to use the bot",
+            switch_pm_parameter="subscribe",
+        )
         return
 
     results = []
-    if '|' in query.query:
-        string, file_type = query.query.split('|', maxsplit=1)
+
+    # Get search query
+    if "|" in query.query:
+        string, file_type = query.query.split("|", maxsplit=1)
         string = string.strip()
         file_type = file_type.strip().lower()
     else:
         string = query.query.strip()
         file_type = None
 
-    offset = int(query.offset or 0)
-    reply_markup = get_reply_markup(query=string)
-    files, next_offset, total = await get_search_results(string,
-                                                  file_type=file_type,
-                                                  max_results=10,
-                                                  offset=offset)
+    # Don't search empty query
+    if not string:
+        await query.answer(
+            results=[],
+            cache_time=0,
+            switch_pm_text="Type a movie name to search",
+            switch_pm_parameter="start",
+        )
+        return
 
+    # Pagination offset
+    try:
+        offset = int(query.offset or 0)
+    except ValueError:
+        offset = 0
+
+    # Search button
+    reply_markup = get_reply_markup(query=string)
+
+    # Search database
+    try:
+        files, next_offset, total = await get_search_results(
+            string,
+            file_type=file_type,
+            max_results=10,
+            offset=offset,
+        )
+    except Exception as e:
+        logger.exception("Search error: %s", e)
+
+        await query.answer(
+            results=[],
+            cache_time=0,
+            switch_pm_text="Search error",
+            switch_pm_parameter="error",
+        )
+        return
+
+    # Create inline results
     for file in files:
-        title=file.file_name
-        size=get_size(file.file_size)
-        f_caption=file.caption
+
+        title = file.file_name
+        size = get_size(file.file_size)
+        f_caption = file.caption
+
+        # Custom caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption=CUSTOM_FILE_CAPTION.format(file_name= '' if title is None else title, file_size='' if size is None else size, file_caption='' if f_caption is None else f_caption)
+                f_caption = CUSTOM_FILE_CAPTION.format(
+                    file_name="" if title is None else title,
+                    file_size="" if size is None else size,
+                    file_caption="" if f_caption is None else f_caption,
+                )
             except Exception as e:
                 logger.exception(e)
-                f_caption=f_caption
+                f_caption = file.caption
+
         if f_caption is None:
-            f_caption = f"{file.file_name}"
+            f_caption = file.file_name
+
         results.append(
             InlineQueryResultCachedDocument(
                 title=file.file_name,
                 document_file_id=file.file_id,
                 caption=f_caption,
-                description=f'Size: {get_size(file.file_size)}\nType: {file.file_type}',
-                reply_markup=reply_markup))
+                description=(
+                    f"Size: {get_size(file.file_size)}\n"
+                    f"Type: {file.file_type}"
+                ),
+                reply_markup=reply_markup,
+            )
+        )
 
+    # Results found
     if results:
-        switch_pm_text = f"{emoji.FILE_FOLDER} Results "
+        switch_pm_text = f"{emoji.FILE_FOLDER} Results"
+
         if string:
             switch_pm_text += f" for {string}"
+
         try:
-            await query.answer(results=results,
-                           is_personal = True,
-                           cache_time=cache_time,
-                           switch_pm_text=switch_pm_text,
-                           switch_pm_parameter="start",
-                           next_offset=str(next_offset))
+            await query.answer(
+                results=results,
+                is_personal=True,
+                cache_time=cache_time,
+                switch_pm_text=switch_pm_text,
+                switch_pm_parameter="start",
+                next_offset=str(next_offset),
+            )
+
         except QueryIdInvalid:
             pass
+
         except Exception as e:
-            logging.exception(str(e))
+            logger.exception("Inline answer error: %s", e)
+
+    # No results
     else:
-        switch_pm_text = f'{emoji.CROSS_MARK} No results'
+        switch_pm_text = f"{emoji.CROSS_MARK} No results"
+
         if string:
             switch_pm_text += f' for "{string}"'
 
-        await query.answer(results=[],
-                           is_personal = True,
-                           cache_time=cache_time,
-                           switch_pm_text=switch_pm_text,
-                           switch_pm_parameter="okay")
+        await query.answer(
+            results=[],
+            is_personal=True,
+            cache_time=cache_time,
+            switch_pm_text=switch_pm_text,
+            switch_pm_parameter="okay",
+        )
 
 
 def get_reply_markup(query):
     buttons = [
         [
-            InlineKeyboardButton('Search again', switch_inline_query_current_chat=query)
+            InlineKeyboardButton(
+                "🔎 Search again",
+                switch_inline_query_current_chat=query,
+            )
         ]
-        ]
+    ]
+
     return InlineKeyboardMarkup(buttons)
-
-
-
-
